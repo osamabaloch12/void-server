5d817c9ea4b42e55158430e01599b12bdb66f830
Morgan Brown <brown.mhg@gmail.com>
2013-06-06 17:53:40 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Void Web Service
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Void::Application.config.secret_token = 'f4419cca0688582fb31c68ebf2d2c82e687ef06e4a91f6e5e8544b60c2ea14218cba2446d32730be12248a375245534eb1f6ce154d889c7187661bdb478fd8f5'
