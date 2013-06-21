a661f3476b9c45b5d8974986392a67ebbc17f133
Morgan Brown <brown.mhg@gmail.com>
2013-06-20 22:23:17 -0700

    Make image, location, and message attars accessible

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,8 @@
 class Post < ActiveRecord::Base
   has_many :users, :through => :users_posts
 
+  attr_accessible :image, :location, :message
+
   has_attached_file :image, styles: {
     thumb: '720x720!'
   }
