78cbeab1cc0b37308f7aebe74c49825308a0f07c
Morgan Brown <brown.mhg@gmail.com>
2013-06-21 00:10:35 -0700

    So apparently you also need a path...

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,7 @@ Void::Application.configure do
   config.paperclip_defaults = {
     :storage => :s3,
     :url => ":s3_domain_url",
+    :path => "/:class/:attachment/:id_partition/:style/:filename"
     :s3_credentials => {
       :bucket => ENV['AWS_BUCKET'],
       :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
